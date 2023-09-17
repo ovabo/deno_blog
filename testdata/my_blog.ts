60edb3f3d1164364d04bf1e9e01e8f7025f525cf
ovabo <120366266+ovabo@users.noreply.github.com>
2023-09-17 06:11:48 -0400

    Update my_blog.ts

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -2,7 +2,7 @@ import blog from "../blog.tsx";
 
 blog({
   author: "alexander",
-  title: "Beyond the Seen"",
+  title: "Beyond the Seen",
   description: "From Tangible Threats to Metaphysical Battles.",
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
