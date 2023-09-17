1286ba8d1bf3a4ff03ccc7da7563a230a9f08351
ovabo <120366266+ovabo@users.noreply.github.com>
2023-09-17 06:08:14 -0400

    Update my_blog.ts

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -1,14 +1,14 @@
 import blog from "../blog.tsx";
 
 blog({
-  author: "Dino",
-  title: "My Blog",
-  description: "The blog description.",
+  author: "alexander",
+  title: "Beyond the Seen"",
+  description: "From Tangible Threats to Metaphysical Battles.",
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
   links: [
-    { title: "bot@deno.com", url: "mailto:bot@deno.com" },
-    { title: "GitHub", url: "https://github.com/denobot" },
-    { title: "Twitter", url: "https://twitter.com/denobot" },
+    { title: "bot@deno.com", url: "mailto:bot@gh.com" },
+    { title: "GitHub", url: "https://github.com/" },
+    { title: "Twitter", url: "https://twitter.com/" },
   ],
 });
