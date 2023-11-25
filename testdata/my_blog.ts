135b2c9c0ac18c0e7f37aff932a4becec816ef9f
ovabo <120366266+ovabo@users.noreply.github.com>
2023-11-24 21:44:03 -0500

    Update my_blog.ts

--- testdata/my_blog.ts
+++ testdata/my_blog.ts
@@ -1,14 +1,29 @@
-import blog from "../blog.tsx";
+import blog, { ga, redirects } from "https://deno.land/x/blog/blog.tsx";
+import { unocss_opts } from "./unocss.ts";
 
 blog({
-  author: "alexander",
-  title: "Beyond the Seen",
-  description: "From Tangible Threats to Metaphysical Battles.",
-  avatar: "Alex-Ovabor-Green.png",
-  avatarClass: "rounded-full",
+  author: "Dino",
+  title: "My Blog",
+  description: "The blog description.",
+  avatar: "avatar.png",
+  avatarClass: "rectangle-full",
   links: [
-    { title: "bot@deno.com", url: "mailto:bot@ghb.com" },
-    { title: "GitHub", url: "https://github.com/" },
-    { title: "Twitter", url: "https://twitter.com/" },
+    { title: "Email", url: "mailto:bot@deno.com" },
+    { title: "GitHub", url: "https://github.com/denobot" },
+    { title: "Twitter", url: "https://twitter.com/denobot" },
   ],
+  lang: "en",
+  // localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+  dateFormat: (date) =>
+    new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
+  middlewares: [
+    ga("UA-XXXXXXXX-X"),
+    redirects({
+      "/foo": "/my_post",
+      // you can skip leading slashes too
+      "bar": "my_post2",
+    }),
+  ],
+  unocss: unocss_opts, // check https://github.com/unocss/unocss
+  favicon: "favicon.ico",
 });
